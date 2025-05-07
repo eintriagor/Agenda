f7ba21f848e017a21e159756c87ade35b432196d
eintriagor <edivico@gmail.com>
2025-05-07 17:30:24 +0200

    Control d’errors per camps buits al formulari

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const contactes = ["Jose","Saida","Ruben"];
 		llista.appendChild(li);
 	});
 	document.body.appendChild(llista);
+
+document.querySelector("form").addEventListener("submit", e => {
+
+	e.preventDefault();
+	const input = document.querySelector("#nom");
+	if (input.value.trim() === "") {
+		alert("El camp no pot estar buit");
+		return;
+	}
+	// Lògica per afegir el contacte...
+});
